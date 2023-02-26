41d82aaf9a1b8f007aa3b2361889af7438a28159
minh152Dlz <99807270+minh152Dlz@users.noreply.github.com>
2023-02-27 00:28:55 +0700

    update add article
    update8

--- btth01/admin/process_edit_article.php
+++ btth01/admin/process_edit_article.php
@@ -8,7 +8,15 @@
     $tomtat= $_POST['txttomtat'];
     $noidung= $_POST['txtnoidung'];
     $matgia = $_POST['txttgia'];
-    $sql = "UPDATE baiviet SET `tieude` = '$tieude', `ten_bhat` = '$tenbhat', `ma_tloai` = '$matloai', `tomtat` = '$tomtat', `noidung` = '$noidung', `ma_tgia` = '$matgia'
+    $ngayviet = date('Y-m-d H:i:s');
+    $hinhanh = $_POST['file-upload'];
+
+        $link = './images/songs/' . $_FILES['file-upload']['name'];
+        $temp = $_FILES['file-upload']['tmp_name'];
+        $path = '../images/songs/' . $_FILES['file-upload']['name'];
+        $moved = move_uploaded_file($temp, $path);
+
+    $sql = "UPDATE baiviet SET `tieude` = '$tieude', `ten_bhat` = '$tenbhat', `ma_tloai` = '$matloai', `tomtat` = '$tomtat', `noidung` = '$noidung', `ma_tgia` = '$matgia',`ngayviet` ='$ngayviet'
     WHERE ma_bviet = '$mabviet'";
     $result = mysqli_query($conn,$sql);
     header('Location: article.php');
